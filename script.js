1a4b3c81ec0c5ef698bc1212fe2617c71b3d2af9
ruhit1000 <ruhitpaul18@gmail.com>
2026-02-21 22:13:51 +0600

    all jobs rendering added

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const rejectedJobCount = document.getElementById('rejected-job-count');
 const filterButtons = document.getElementsByClassName('filter-btn');
 const filterJobCounter = document.getElementById('filter-jobs-counter');
 const filterJobText = document.getElementById('filter-job-text');
+const noInterviewCard = document.getElementById('no-interview-card');
+const noRejectedCard = document.getElementById('no-rejected-card');
 
 
 // all counter update
@@ -18,9 +20,44 @@ rejectedJobCount.innerText = allRejectedPosts.length;
 
 // filter button functionality
 for (const filterBtn of filterButtons) {
-    filterBtn.addEventListener('click', ()=>{
+    filterBtn.addEventListener('click', () => {
         updateActiveStat(filterBtn, filterButtons);
         const buttonName = filterBtn.innerText;
         changeSection(buttonName);
     })
+}
+
+
+// function for zero interview and zero rejected
+if (allInterviewPosts.length == 0) {
+    noInterviewCard.classList.remove('hidden');
+} else {
+    noInterviewCard.classList.add('hidden');
+}
+if (allRejectedPosts.length == 0) {
+    noRejectedCard.classList.remove('hidden');
+} else {
+    noRejectedCard.classList.add('hidden');
+};
+
+
+// all job posts rendering
+for (const jobPost of allJobPosts) {
+    const jobPostCard = document.createElement('div');
+    jobPostCard.classList = 'job-card flex justify-between bg-white p-6 rounded-lg border border-base-300';
+    jobPostCard.innerHTML = `
+    <div>
+        <h2 class="text-[#002c5c] font-semibold text-lg">${jobPost.company_name}</h2>
+        <p class="text-gray-500">${jobPost.position}</p>
+        <p class="my-5 text-gray-500">${jobPost.work_location} | ${jobPost.job_type} | ${jobPost.salary_range}</p>
+        <div class="badge badge-neutral badge-outline p-3 mb-2">NOT APPLIED</div>
+        <P class="text-[#323B49] mb-5">${jobPost.discipline}</P>
+        <button class="btn btn-outline btn-success">INTERVIEW</button>
+        <button class="btn btn-outline btn-error ml-2">REJECTED</button>
+    </div>
+    <button class="btn rounded-full">
+        <i class="fa-regular fa-trash-can"></i>
+    </button>
+    `
+    allJobsContainer.append(jobPostCard);
 }
