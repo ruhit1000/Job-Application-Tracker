ec7803b4f1df8d7cfca97704e529c927917a6039
ruhit1000 <ruhitpaul18@gmail.com>
2026-02-22 12:37:16 +0600

    section filtering updated, card badge updated

--- machine.js
+++ machine.js
@@ -3,6 +3,16 @@ function countTotal() {
     totalJobCount.innerText = allJobPosts.length;
     interviewJobCount.innerText = allInterviewPosts.length;
     rejectedJobCount.innerText = allRejectedPosts.length;
+    if (allInterviewPosts.length == 0) {
+        noInterviewCard.classList.remove('hidden');
+    } else {
+        noInterviewCard.classList.add('hidden');
+    }
+    if (allRejectedPosts.length == 0) {
+        noRejectedCard.classList.remove('hidden');
+    } else {
+        noRejectedCard.classList.add('hidden');
+    };
 }
 
 // filter button active status change
@@ -39,7 +49,29 @@ function changeSection(buttonName) {
 
 // add jobCard to interview array
 function addJobCardToInterview(buttonElement) {
-    const cardElement = buttonElement.parentElement;
+    const matchedJobPost = getJobDetailsObject(buttonElement)
+    if (!allInterviewPosts.includes(matchedJobPost)) {
+        allInterviewPosts.push(matchedJobPost);
+    }
+    allRejectedPosts = allRejectedPosts.filter((object)=>{
+        return !(object.company_name === matchedJobPost.company_name && object.position === matchedJobPost.position);
+    })
+};
+
+// add jobCard to rejected array
+function addJobCardToRejected(buttonElement) {
+    const matchedJobPost = getJobDetailsObject(buttonElement);
+    if (!allRejectedPosts.includes(matchedJobPost)) {
+        allRejectedPosts.push(matchedJobPost);
+    }
+    allInterviewPosts = allInterviewPosts.filter((object)=>{
+        return !(object.company_name === matchedJobPost.company_name && object.position === matchedJobPost.position);
+    })
+};
+
+// find out the object from all jobs array
+function getJobDetailsObject(targetButton) {
+    const cardElement = targetButton.parentElement;
     const companyName = cardElement.querySelector('h2').innerText;
     const positionName = cardElement.querySelector('.job-position').innerText;
     let matchedJobPost = null;
@@ -48,19 +80,41 @@ function addJobCardToInterview(buttonElement) {
             matchedJobPost = jobPost;
         }
     };
-    if (!allInterviewPosts.includes(matchedJobPost)) {
-        allInterviewPosts.push(matchedJobPost);
-    }
-    allRejectedPosts.filter((object) => {
-        return object !== matchedJobPost;
-    })
-};
+    return matchedJobPost;
+}
 
 // update card status
-function updateCardStatus(buttonElement){
+function updateCardStatus(buttonElement) {
     const cardElement = buttonElement.parentElement;
     const badge = cardElement.querySelector('.badge')
     badge.innerText = buttonElement.innerText;
-    badge.classList.remove('badge-neutral', 'badge-outline');
-    badge.classList.add('badge-success');
-}
+    badge.classList.remove('badge-neutral', 'badge-outline', 'badge-success', 'badge-error');
+    if (buttonElement.innerText === 'INTERVIEW') {
+        badge.classList.add('badge-success');
+    } else {
+        badge.classList.add('badge-error');
+    };
+};
+
+// render job post cards
+function renderJobCards(jobCardsArray, jobCardsContainer) {
+    for (const jobPost of jobCardsArray) {
+        const jobPostCard = document.createElement('div');
+        jobPostCard.classList = 'job-card flex justify-between bg-white p-6 rounded-lg border border-base-300';
+        jobPostCard.innerHTML = `
+    <div>
+        <h2 class="text-[#002c5c] font-semibold text-lg">${jobPost.company_name}</h2>
+        <p class="job-position text-gray-500">${jobPost.position}</p>
+        <p class="my-5 text-gray-500">${jobPost.work_location} | ${jobPost.job_type} | ${jobPost.salary_range}</p>
+        <div class="badge badge-neutral badge-outline p-3 mb-2">NOT APPLIED</div>
+        <P class="text-[#323B49] mb-5">${jobPost.discipline}</P>
+        <button class="btn btn-outline btn-success">INTERVIEW</button>
+        <button class="btn btn-outline btn-error ml-2">REJECTED</button>
+    </div>
+    <button class="btn rounded-full">
+        <i class="fa-regular fa-trash-can"></i>
+    </button>
+    `
+        jobCardsContainer.append(jobPostCard);
+    };
+};
--- script.js
+++ script.js
@@ -22,44 +22,11 @@ for (const filterBtn of filterButtons) {
         updateActiveStat(filterBtn, filterButtons);
         const buttonName = filterBtn.innerText;
         changeSection(buttonName);
-    })
-}
-
-
-// function for zero interview and zero rejected
-if (allInterviewPosts.length == 0) {
-    noInterviewCard.classList.remove('hidden');
-} else {
-    noInterviewCard.classList.add('hidden');
-}
-if (allRejectedPosts.length == 0) {
-    noRejectedCard.classList.remove('hidden');
-} else {
-    noRejectedCard.classList.add('hidden');
+    });
 };
-
 
 // all job posts rendering
-for (const jobPost of allJobPosts) {
-    const jobPostCard = document.createElement('div');
-    jobPostCard.classList = 'job-card flex justify-between bg-white p-6 rounded-lg border border-base-300';
-    jobPostCard.innerHTML = `
-    <div>
-        <h2 class="text-[#002c5c] font-semibold text-lg">${jobPost.company_name}</h2>
-        <p class="job-position text-gray-500">${jobPost.position}</p>
-        <p class="my-5 text-gray-500">${jobPost.work_location} | ${jobPost.job_type} | ${jobPost.salary_range}</p>
-        <div class="badge badge-neutral badge-outline p-3 mb-2">NOT APPLIED</div>
-        <P class="text-[#323B49] mb-5">${jobPost.discipline}</P>
-        <button class="btn btn-outline btn-success">INTERVIEW</button>
-        <button class="btn btn-outline btn-error ml-2">REJECTED</button>
-    </div>
-    <button class="btn rounded-full">
-        <i class="fa-regular fa-trash-can"></i>
-    </button>
-    `
-    allJobsContainer.append(jobPostCard);
-};
-
+renderJobCards(allJobPosts, allJobsContainer);
 
 // Interview and Rejected button functionality
 mainContainer.addEventListener('click', (event)=>{
@@ -70,4 +37,12 @@ mainContainer.addEventListener('click', (event)=>{
         updateCardStatus(buttonElement);
         countTotal();
     }
+    if (buttonName === 'REJECTED') {
+        addJobCardToRejected(buttonElement);
+        updateCardStatus(buttonElement);
+        countTotal();
+    };
 })
+
+// renderJobCards(allInterviewPosts, allInterviewContainer);
+// renderJobCards(allRejectedPosts, allRejectedContainer);
